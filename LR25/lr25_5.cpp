1cd7d69f40f96deae62d3f90cfaf4f79af141956
Denys <denissinko2004@gmail.com>
2025-02-27 23:49:35 +0200

    upd pr25

--- LR25/lr25_5.cpp
+++ LR25/lr25_5.cpp
@@ -1,43 +1,42 @@
 #include <iostream>
 using namespace std;
 
-int main (){
+int main (){ 
     int i, n, j, s=0;
     cout<<"Enter N: ";
     cin>>n;
-    int a[n*2], *pa, *pj, *pj1, *ps;
-    pa=a;
+    int a[n*2], *pa, *pj, *ps; //оголошуємо масив і необхідні показники
+    pa=a; //присвоюємо показнику перший ел-т масиву
     cout<<"Enter numbers: "<<endl;
-    for(i=0;i<n;i++){
+    for(i=0;i<n;i++){ //вводимо вручну елементи
         cin>>*pa;
-        pa++;
+        pa++; //зсув показника
     }
 
     pa=a;
-    for(i=0;i<n;i++){
-        if(*pa%5==0){
+    for(i=0;i<n;i++){ //цикл для знаходження ел-тів %5
+        if(*pa%5==0){ //перевірка умови %5
             ps=a;
             s=0;
-            // for(j=0;j<i;j++){
-            //     s=s+*ps;
-            //     ps++;
-            // }
-            for(j=n;j>i+1;j--){
-                // pj=&a[j];
-                // pj1=pj-1;
-                // *pj=*pj1;
-                // pj--;
-                a[j]=a[j-1];
+            for(j=0;j<i;j++){ //сумма ел-тів, що стоять перед ел-том, що відповідає умові
+                s=s+*ps;
+                ps++;
+            }
+            for(j=n;j>i+1;j--){ //цикл для вставки елемента
+                pj=&a[j];
+                *pj=*(pj-1);
+                pj--;
             }
             pa++;
-            *pa=100;
+            i++;
+            *pa=s; //вставка суми
             n++;
         }
         pa++;
     }
     cout<<endl;
     pa=a;
-    for(i=0;i<n;i++){
+    for(i=0;i<n;i++){ //виведення масиву
         cout<<*pa<<" ";
         pa++;
     }
